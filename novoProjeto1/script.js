8107f1d1b6e9135e45a758dd2039596c55f8a182
Marcelo Xavier <marcoliveira73@gmail.com>
2021-06-23 17:02:37 -0300

    Primeiros arquivos criados no curso Js

---
+++ novoProjeto1/script.js
@@ -0,0 +1,19 @@
+//Primeiro código Js do curso
+
+console.log ("Olá Mundo Js!");
+
+let valor1 = 10;
+let valor2 = 20;
+let valor3 = "3";
+
+function somarValores(primeiro, segundo){
+    console.log(primeiro + segundo);
+}
+
+//O que ocorrerá nos seguintes casos
+
+somarValores(9, valor1);
+somarValores(valor1 , valor2);
+somarValores(valor2 , valor3);
+somarValores(valor2 , valor3);
+somarValores("100", valor3);
